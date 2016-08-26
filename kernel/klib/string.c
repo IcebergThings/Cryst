f84ce6645b605dfe1cf67bd547c7ab835aa2fa09
bobcao3 <bobcaocheng@163.com>
2016-08-26 09:04:24 +0800

    修正格式

--- kernel/klib/string.c
+++ kernel/klib/string.c
@@ -1,3 +1,22 @@
+// Copyright 2016 Iceberg OS
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//=============================================================================
+// ■ string.c
+//-----------------------------------------------------------------------------
+//   内核字符串库
+//=============================================================================
+
 char* itoa(int value, char* result, int base) {
  // check that the base if valid
  if (base < 2 || base > 36) { *result = '\0'; return result; }
